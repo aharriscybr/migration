bc65f43dbf91653f35ef76b20b5a37336fdb6337
Andrew Harris <andrew.harris@cyberark.com>
2025-07-17 14:16:07 -0400

    host comments

--- utility/policy-gen/host.ps1
+++ utility/policy-gen/host.ps1
@@ -7,17 +7,35 @@ function Gen-Workload ( $workload ) {
     $wid = $workload.id
     $whitespace     = ""
 
+    # create array of object, parsing out colon separated objects, convert to array
     $workloadEntityID = parseEntity -e $wid -s ":"
+
+
     $clean_wid = $workloadEntityID[2] | ForEach-Object { $_ -replace "/", "."}
 
+    # Capture workload annotations
     $wid_anno = $workload.annotations
+
+    # last index of this array is the path/to/workload/id
+    # first index is conjur_account
+    # second index is type of resource
+    # conjur_account:kind:path/to/workload/id
+    # conjur_account kind path/to/workload/id
     $wid_name = $workloadEntityID[2]
 
+    # Capture owning policy member
     $wid_base = $workload.policy
 
+    # Create clean base to trim workloads loaded into a specific path
+    # this is used in authenticators, i.e. github:
+    # conjur_account:host:github-branch/repository_claim
     $wid_base_clean = parseEntity -e $wid_base -s ":"
     $wid_base_id = $wid_base_clean[2]
 
+    # Clean workload name up for use
+    # remove the policy it was loaded into
+    # trim `/` from prefix if loaded
+    # if loaded into root without, throw these values away
     $wid_name_clean = $wid_name.TrimStart($wid_base_id)
     $wid_name_clean = $wid_name_clean.TrimStart('/')
 
@@ -32,14 +50,16 @@ function Gen-Workload ( $workload ) {
     Add-Content $resourceFileTemp -Value "# CLI Example load - conjur policy load -b $wid_base_id -f $thisResourceFile"
 
     Add-Content $resourceFileTemp -Value "- !host"
-    
+
     # Check if in root (data) branch, if so give default name, not clean.
     if ( $wid_base_id -eq "root" )
     {
+        # If loaded into root
         Add-Content $resourceFileTemp -Value "  id: $wid_name"
     }
     else
     {
+        # handle from parsed variable above
         Add-Content $resourceFileTemp -Value "  id: $wid_name_clean"
     }
 
